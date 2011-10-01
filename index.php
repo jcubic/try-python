92fde082568557f88ffd3453c143840dff6c5455
Jakub Jankiewicz <jcubic@onet.pl>
2011-10-01 15:29:08 +0200

    Clean up index.php

--- index.php
+++ index.php
@@ -21,10 +21,8 @@ if (strcmp($_SERVER['HTTP_HOST'], 'localhost') != 0) {
     <script src="http://terminal.jcubic.pl/js/jquery.terminal-min.js"></script>
     <script src="js/main.js"></script>
     <link href="css/style.css" rel="stylesheet"/>
-    <script></script>
 </head>
 <body>
-  <a href="https://github.com/jcubic/try-python" style="position: absolute; top: 0; left: 0; z-index:1000"><img style="border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/bec6c51521dcc8148146135149fe06a9cc737577/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork Try Python on GitHub"></a>
   <section>
     <header><img src="css/python.png"/><h1>Try Python</h1></header>
     <div id="terminal"></div>
@@ -46,6 +44,7 @@ if (strcmp($_SERVER['HTTP_HOST'], 'localhost') != 0) {
     </div>
   </aside>
   <footer>Copyright &copy; <?php echo date('Y'); ?> <a href="http://jcubic.pl">Jakub Jankiewicz</a></footer>
+  <a href="https://github.com/jcubic/try-python" style="position: absolute; top: 0; left: 0; z-index:1000"><img style="border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/bec6c51521dcc8148146135149fe06a9cc737577/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork Try Python on GitHub"></a>
 
 </body>
 </html>
